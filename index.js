4460d381dcaa57c8c8942f964d1e579a96ffa200
free-writer-5650 <miles.higbie@gmail.com>
2019-10-03 00:17:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function shout(string) {
   return string.toUpperCase()
-}
+};
 
 function whisper(string){
   return string.toLowerCase()
-}
+};
 
 function shout(string) {
   return console.log(string.toUpperCase())
-}
+};
